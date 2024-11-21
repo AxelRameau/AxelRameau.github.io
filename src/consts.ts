40affee89d4c3a2ea9577917e5673c81a8566767
Axel <axel.a.rameau@gmail.com>
2024-11-21 08:44:30 +0100

    envoi du dossier initial

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Axel RAMEAU';
+export const SITE_DESCRIPTION = "Salut, si tu es ici c'est que tu as entendu parlé du crack du design que je suis. Alors bienvenue, j'espère que t'apprécieras mes projets ";
