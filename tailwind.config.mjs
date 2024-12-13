ffb64aa7be5b2f8be2ca0992e319d201ec89b5dd
Axel <axel.a.rameau@gmail.com>
2024-12-13 12:32:05 +0100

    màj avec la version mobile accueil

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'titre': ['Bebas'],
-				'creative': ['Ingrid'],
-				'corps': 'var(--ff-rm)',
-			  },
-			  colors:{
-				"rouge": "var(--clr-rouge)",
-				"beige": "var(--clr-beige)",
-				"noir": "var(--clr-noir)"
-				},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        titre: ["Bebas"],
+        creative: ["Ingrid"],
+        corps: "var(--ff-rm)",
+      },
+      colors: {
+        rouge: "var(--clr-rouge)",
+        beige: "var(--clr-beige)",
+        noir: "var(--clr-noir)",
+      },
+      backgroundImage: {
+        "gallerie1": "url('img-portfolio/bmw-axel-concours.jpg')",
+        "gallerie2": "url('img-portfolio/chag-blanc-post.jpg')",
+		    "gallerie3": "url('img-portfolio/cherie.jpg')",
+		    "gallerie4": "url('img-portfolio/fredo_chien.jpg')",
+		    "gallerie5": "url('img-portfolio/lily_miroir.jpg')",
+        "moi": "url('moi-classe.jpg')",
+      },
+    },
+  },
+
+  plugins: [],
+};
